5996250a50e57f813b9fc059e1c04923f2e0734e
Etnath <kevin.giffone@gmail.com>
2016-11-13 18:05:45 +0100

    [Presentation] Display rss content. Still need to figure out how to style the WPF webbrowser component.

---
+++ GreenFeed/GreenFeed.WPF/Dependencies/BrowserBindableProperties.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GreenFeed.WPF.Dependencies
+{
+    public class BrowserBindableProperties
+    {
+        public static readonly DependencyProperty UrlLinkProperty = DependencyProperty.RegisterAttached(
+            "UrlLink", 
+            typeof(string), 
+            typeof(BrowserBindableProperties), 
+            new FrameworkPropertyMetadata(null, OnUrlChanged));
+
+        [AttachedPropertyBrowsableForType(typeof(WebBrowser))]
+        public static string GetUrlLink(DependencyObject obj)
+        {
+            return (string)obj.GetValue(UrlLinkProperty);
+        }
+
+        public static void SetUrlLink(DependencyObject obj, string value)
+        {
+            obj.SetValue(UrlLinkProperty, value);
+        }
+
+        public static void OnUrlChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
+        {
+            WebBrowser webBrowser = dependencyObject as WebBrowser;
+            string url = e.NewValue as string;
+            if (webBrowser != null)
+            {
+                webBrowser.Source = !String.IsNullOrEmpty(url) ? new Uri(url) : null;
+            }
+        }
+    }
+}
--- GreenFeed/GreenFeed.WPF/Model/NotifiedSyndicationItem.cs
+++ GreenFeed/GreenFeed.WPF/Model/NotifiedSyndicationItem.cs
@@ -11,21 +11,43 @@ namespace GreenFeed.WPF.Model
     public class NotifiedSyndicationItem : NotifyPropertyChangedBase
     {
         private string _title;
-
         public string Title
         {
             get { return _title; }
             set { _title = value;  OnPropertyChanged(); }
         }
 
+        private string _content;
+        public string Content
+        {
+            get { return _content; }
+            set { _content = value; OnPropertyChanged(); }
+        }
+
+        private string _url;
+
+        public string Url
+        {
+            get { return _url; }
+            set { _url = value; OnPropertyChanged(); }
+        }
+
+
         public NotifiedSyndicationItem()
         {
             _title = string.Empty;
+            _content = string.Empty;
         }
 
         public NotifiedSyndicationItem(SyndicationItem item)
         {
             Title = item.Title.Text;
+            Url = item.Id;
+            if (item.Content != null
+                && item.Content.Type.Equals("html", StringComparison.InvariantCultureIgnoreCase))
+            {
+                _content = ((TextSyndicationContent)item.Content)?.Text;
+            }
         }
     }
 }
